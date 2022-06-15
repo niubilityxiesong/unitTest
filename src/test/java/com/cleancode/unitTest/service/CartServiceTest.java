404c43ac3c7bed34f23ff2ade30357da0e1abd31
xiesong <563527588@qq.com>
2022-06-15 21:32:19 +0800

    add test

--- src/test/java/com/cleancode/unitTest/service/CartServiceTest.java
+++ src/test/java/com/cleancode/unitTest/service/CartServiceTest.java
@@ -2,12 +2,12 @@ package com.cleancode.unitTest.service;
 
 import com.cleancode.unitTest.entity.Customer;
 import com.cleancode.unitTest.entity.Item;
+import com.cleancode.unitTest.exception.CustomerDoNotExistException;
 import com.cleancode.unitTest.exception.ItemNotValidException;
 import com.cleancode.unitTest.module.ItemDto;
 import com.cleancode.unitTest.module.ItemTotalPriceDto;
 import com.cleancode.unitTest.repository.ItemRepository;
 import com.cleancode.unitTest.service.mapper.ItemMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,12 +15,14 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -38,6 +40,17 @@ public class CartServiceTest {
 
     @InjectMocks
     private CartService cartService;
+
+    @Test
+    void should_throw_exception_when_customer_validation_get_error() {
+        int customerId = 1;
+        ItemDto itemDto = ItemDto
+                .builder()
+                .build();
+        doThrow(CustomerDoNotExistException.class).when(customerService).isValidUser(customerId);
+
+        assertThrows(CustomerDoNotExistException.class, () -> cartService.setItem(customerId, itemDto));
+    }
 
     @Test
     void should_throw_exception_when_itemDto_has_user_id() {
@@ -79,7 +92,7 @@ public class CartServiceTest {
     }
 
     @Test
-    void should_return_total_price_0_when_user_without_items() {
+    void should_return_total_price_0_when_user_without_item() {
         int customerId = 1;
         final Customer customer = Customer.builder().id(customerId).build();
         when(customerService.getCustomer(customerId)).thenReturn(customer);
@@ -88,6 +101,130 @@ public class CartServiceTest {
         final ItemTotalPriceDto totalPriceDto = cartService.getTotalPrice(customerId);
 
         assertThat(totalPriceDto.getTotalPrice()).isEqualTo(BigDecimal.ZERO);
+        assertThat(totalPriceDto.getDiscountPrice()).isEqualTo(BigDecimal.ZERO);
+        assertThat(totalPriceDto.getOriginalPrice()).isEqualTo(BigDecimal.ZERO);
+    }
+
+    @Test
+    void should_return_total_price_dto_without_any_discount_when_user_with_item_and_without_vip() {
+        int customerId = 1;
+        final Customer customer = Customer.builder().id(customerId).isVip(false).build();
+        when(customerService.getCustomer(customerId)).thenReturn(customer);
+        Item item = Item.
+                builder()
+                .unitPrice(new BigDecimal("1000"))
+                .discount(1.0)
+                .build();
+        when(itemRepository.findByCustomerId(customer.getId()))
+                .thenReturn(Optional.of(Collections.singletonList(item)));
+
+        final ItemTotalPriceDto totalPriceDto = cartService.getTotalPrice(customerId);
+
+        assertThat(totalPriceDto.getTotalPrice().compareTo(new BigDecimal("1000"))).isEqualTo(0);
+        assertThat(totalPriceDto.getOriginalPrice().compareTo(new BigDecimal("1000"))).isEqualTo(0);
+        assertThat(totalPriceDto.getDiscountPrice().compareTo(new BigDecimal("0"))).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_total_price_dto_with_item_discount_when_user_with_item_and_without_vip() {
+        int customerId = 1;
+        final Customer customer = Customer
+                .builder()
+                .id(customerId)
+                .isVip(false)
+                .build();
+        when(customerService.getCustomer(customerId)).thenReturn(customer);
+        Item item = Item.
+                builder()
+                .unitPrice(new BigDecimal("1000"))
+                .discount(0.9)
+                .build();
+        when(itemRepository.findByCustomerId(customer.getId()))
+                .thenReturn(Optional.of(Collections.singletonList(item)));
+
+        final ItemTotalPriceDto totalPriceDto = cartService.getTotalPrice(customerId);
+
+        assertThat(totalPriceDto.getTotalPrice().compareTo(new BigDecimal("900"))).isEqualTo(0);
+        assertThat(totalPriceDto.getOriginalPrice().compareTo(new BigDecimal("1000"))).isEqualTo(0);
+        assertThat(totalPriceDto.getDiscountPrice().compareTo(new BigDecimal("100"))).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_total_price_dto_with_vip_discount_when_user_with_item_and_with_vip() {
+        int customerId = 1;
+        final Customer customer = Customer
+                .builder()
+                .id(customerId)
+                .isVip(true)
+                .build();
+        when(customerService.getCustomer(customerId)).thenReturn(customer);
+        Item item = Item.
+                builder()
+                .unitPrice(new BigDecimal("1000"))
+                .discount(1.0)
+                .build();
+        when(itemRepository.findByCustomerId(customer.getId()))
+                .thenReturn(Optional.of(Collections.singletonList(item)));
+
+        final ItemTotalPriceDto totalPriceDto = cartService.getTotalPrice(customerId);
+
+        assertThat(totalPriceDto.getTotalPrice().compareTo(new BigDecimal("990"))).isEqualTo(0);
+        assertThat(totalPriceDto.getOriginalPrice().compareTo(new BigDecimal("1000"))).isEqualTo(0);
+        assertThat(totalPriceDto.getDiscountPrice().compareTo(new BigDecimal("10"))).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_total_price_dto_with_item_and_vip_discount_when_user_with_item_and_with_vip() {
+        int customerId = 1;
+        final Customer customer = Customer
+                .builder()
+                .id(customerId)
+                .isVip(true)
+                .build();
+        when(customerService.getCustomer(customerId)).thenReturn(customer);
+        Item item = Item.
+                builder()
+                .unitPrice(new BigDecimal("1000"))
+                .discount(0.9)
+                .build();
+        when(itemRepository.findByCustomerId(customer.getId()))
+                .thenReturn(Optional.of(Collections.singletonList(item)));
+
+        final ItemTotalPriceDto totalPriceDto = cartService.getTotalPrice(customerId);
+
+        assertThat(totalPriceDto.getTotalPrice().compareTo(new BigDecimal("890"))).isEqualTo(0);
+        assertThat(totalPriceDto.getOriginalPrice().compareTo(new BigDecimal("1000"))).isEqualTo(0);
+        assertThat(totalPriceDto.getDiscountPrice().compareTo(new BigDecimal("110"))).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_total_price_dto_with_item_and_vip_discount_when_user_with_more_than_1_items_and_with_vip() {
+        int customerId = 1;
+        final Customer customer = Customer
+                .builder()
+                .id(customerId)
+                .isVip(true)
+                .build();
+        when(customerService.getCustomer(customerId)).thenReturn(customer);
+        Item apple = Item.
+                builder()
+                .unitPrice(new BigDecimal("1000"))
+                .discount(0.9)
+                .build();
+        Item book = Item.
+                builder()
+                .unitPrice(BigDecimal.ONE)
+                .discount(1.0)
+                .build();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(apple);
+        items.add(book);
+        when(itemRepository.findByCustomerId(customer.getId())).thenReturn(Optional.of(items));
+
+        final ItemTotalPriceDto totalPriceDto = cartService.getTotalPrice(customerId);
+        assertThat(totalPriceDto.getTotalPrice().compareTo(new BigDecimal("891"))).isEqualTo(0);
+        assertThat(totalPriceDto.getOriginalPrice().compareTo(new BigDecimal("1001"))).isEqualTo(0);
+        assertThat(totalPriceDto.getDiscountPrice().compareTo(new BigDecimal("110"))).isEqualTo(0);
     }
 
 }
